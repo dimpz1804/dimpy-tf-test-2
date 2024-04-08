603d01d600e3c22e44b3787348e3248b748fdc71
dimpz1804 <dimpy.chugh@hashicorp.com>
2024-04-08 14:50:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 # datainn_uae_infra_project_name = var.datainn_uae_infra_project_name
 # datainn_uae_infra_initial_setup_azure_workspace_name = var.datainn_uae_infra_initial_setup_azure_workspace_name
 # }
-
+variable "TF_TOKEN_app_terraform_io"{}
 data "tfe_organization" "organization" {
   name  = "dimpy_test"
 }
